6155d6c3a6d63d4555470f89d2ee2a7b31fdab20
Folf18 <Fedir_@i.ua>
2019-02-05 15:29:23 +0200

    Create shop_data.sql

---
+++ shop_data.sql
@@ -0,0 +1,101 @@
+
+--
+-- TOC entry 2897 (class 0 OID 33007)
+-- Dependencies: 205
+-- Data for Name: category; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.category (id, name) VALUES (1, 'Cheese');
+INSERT INTO public.category (id, name) VALUES (2, 'Dairy products');
+INSERT INTO public.category (id, name) VALUES (3, 'Meat products');
+INSERT INTO public.category (id, name) VALUES (4, 'Seafood');
+INSERT INTO public.category (id, name) VALUES (5, 'Coffee');
+INSERT INTO public.category (id, name) VALUES (6, 'Oil');
+INSERT INTO public.category (id, name) VALUES (7, 'Dries fruit');
+INSERT INTO public.category (id, name) VALUES (8, 'Fruits');
+INSERT INTO public.category (id, name) VALUES (9, 'Drinks');
+
+
+--
+-- TOC entry 2891 (class 0 OID 32974)
+-- Dependencies: 199
+-- Data for Name: city; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.city (id, name, country_id) VALUES (1, 'London', 1);
+INSERT INTO public.city (id, name, country_id) VALUES (2, 'Paris', 2);
+INSERT INTO public.city (id, name, country_id) VALUES (3, 'Madrid', 3);
+INSERT INTO public.city (id, name, country_id) VALUES (4, 'Birmingham', 1);
+INSERT INTO public.city (id, name, country_id) VALUES (5, ' Manchester', 1);
+INSERT INTO public.city (id, name, country_id) VALUES (6, 'Glasgow', 1);
+INSERT INTO public.city (id, name, country_id) VALUES (7, 'Sheffield', 1);
+INSERT INTO public.city (id, name, country_id) VALUES (8, 'Marseille', 2);
+INSERT INTO public.city (id, name, country_id) VALUES (9, 'Toulouse', 2);
+INSERT INTO public.city (id, name, country_id) VALUES (10, 'Bordeaux', 2);
+INSERT INTO public.city (id, name, country_id) VALUES (11, 'Barcelona', 3);
+INSERT INTO public.city (id, name, country_id) VALUES (12, 'Valencia', 3);
+INSERT INTO public.city (id, name, country_id) VALUES (13, 'Seville', 3);
+INSERT INTO public.city (id, name, country_id) VALUES (14, 'Kyiv', 4);
+INSERT INTO public.city (id, name, country_id) VALUES (15, 'Kharkiv', 4);
+INSERT INTO public.city (id, name, country_id) VALUES (16, 'Dnipro', 4);
+INSERT INTO public.city (id, name, country_id) VALUES (17, 'Berlin', 5);
+INSERT INTO public.city (id, name, country_id) VALUES (18, 'Hamburg', 5);
+INSERT INTO public.city (id, name, country_id) VALUES (19, 'Munich', 5);
+INSERT INTO public.city (id, name, country_id) VALUES (20, 'Brussels', 6);
+INSERT INTO public.city (id, name, country_id) VALUES (21, 'Antwerpen', 6);
+INSERT INTO public.city (id, name, country_id) VALUES (22, 'New York City', 7);
+INSERT INTO public.city (id, name, country_id) VALUES (23, 'Los Angeles', 7);
+INSERT INTO public.city (id, name, country_id) VALUES (24, 'Chicago', 7);
+INSERT INTO public.city (id, name, country_id) VALUES (25, 'Katowice', 8);
+INSERT INTO public.city (id, name, country_id) VALUES (26, 'Lublin', 8);
+INSERT INTO public.city (id, name, country_id) VALUES (27, 'Warsaw', 8);
+INSERT INTO public.city (id, name, country_id) VALUES (28, 'Rome', 9);
+INSERT INTO public.city (id, name, country_id) VALUES (29, 'Milan', 9);
+INSERT INTO public.city (id, name, country_id) VALUES (30, 'Naples', 9);
+INSERT INTO public.city (id, name, country_id) VALUES (31, 'Prague', 10);
+INSERT INTO public.city (id, name, country_id) VALUES (32, 'Ostrava', 10);
+INSERT INTO public.city (id, name, country_id) VALUES (33, 'Brno', 10);
+INSERT INTO public.city (id, name, country_id) VALUES (34, 'Moscow', 11);
+INSERT INTO public.city (id, name, country_id) VALUES (35, 'Saint Petersburg', 11);
+INSERT INTO public.city (id, name, country_id) VALUES (36, 'Stockholm', 12);
+INSERT INTO public.city (id, name, country_id) VALUES (37, 'Malmö', 12);
+INSERT INTO public.city (id, name, country_id) VALUES (38, 'Göteborg', 12);
+INSERT INTO public.city (id, name, country_id) VALUES (39, 'Oslo', 13);
+INSERT INTO public.city (id, name, country_id) VALUES (40, 'Bergen', 13);
+INSERT INTO public.city (id, name, country_id) VALUES (41, 'Trondheim', 13);
+INSERT INTO public.city (id, name, country_id) VALUES (42, 'Helsinki', 14);
+INSERT INTO public.city (id, name, country_id) VALUES (43, 'Tampere', 14);
+INSERT INTO public.city (id, name, country_id) VALUES (44, 'Bucharest', 15);
+INSERT INTO public.city (id, name, country_id) VALUES (45, 'Constanta', 15);
+INSERT INTO public.city (id, name, country_id) VALUES (46, 'Cluj-Napoca', 15);
+INSERT INTO public.city (id, name, country_id) VALUES (47, 'Budapest', 16);
+INSERT INTO public.city (id, name, country_id) VALUES (48, 'Debrecen', 16);
+INSERT INTO public.city (id, name, country_id) VALUES (49, 'Szeged', 16);
+INSERT INTO public.city (id, name, country_id) VALUES (50, 'Samara', 11);
+
+
+--
+-- TOC entry 2889 (class 0 OID 32963)
+-- Dependencies: 197
+-- Data for Name: country; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.country (id, name) VALUES (1, 'United Kingdom');
+INSERT INTO public.country (id, name) VALUES (2, 'France');
+INSERT INTO public.country (id, name) VALUES (3, 'Spain');
+INSERT INTO public.country (id, name) VALUES (4, 'Ukraine');
+INSERT INTO public.country (id, name) VALUES (5, 'Germany');
+INSERT INTO public.country (id, name) VALUES (6, 'Belgium');
+INSERT INTO public.country (id, name) VALUES (7, 'USA');
+INSERT INTO public.country (id, name) VALUES (8, 'Poland');
+INSERT INTO public.country (id, name) VALUES (9, 'Italy');
+INSERT INTO public.country (id, name) VALUES (10, 'Czech Republic');
+INSERT INTO public.country (id, name) VALUES (11, 'Russia');
+INSERT INTO public.country (id, name) VALUES (12, 'Sweden');
+INSERT INTO public.country (id, name) VALUES (13, 'Norway');
+INSERT INTO public.country (id, name) VALUES (14, 'Finland');
+INSERT INTO public.country (id, name) VALUES (15, 'Romania');
+INSERT INTO public.country (id, name) VALUES (16, 'Hungary');
+
+
+
